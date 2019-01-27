54b3eb2b967dfbf1d96a137a8a6083e02d75fcab
Nicholas <nicholas.james.manganelli@cern.ch>
2019-01-27 18:04:58 +0100

    Adding Autodictionaries, may be useful

---
+++ ntupler/test/AutoDict_vector_pair_vector_TLorentzVector__vector_int___.cxx
@@ -0,0 +1,14 @@
+#include "TLorentzVector.h"
+#include "vector"
+#include "utility"
+#ifdef __CINT__ 
+#pragma link C++ nestedclasses;
+#pragma link C++ nestedtypedefs;
+#pragma link C++ class vector<pair<vector<TLorentzVector>,vector<int>>>+;
+#pragma link C++ class vector<pair<vector<TLorentzVector>,vector<int>>>::*;
+#ifdef G__VECTOR_HAS_CLASS_ITERATOR
+#pragma link C++ operators vector<pair<vector<TLorentzVector>,vector<int>>>::iterator;
+#pragma link C++ operators vector<pair<vector<TLorentzVector>,vector<int>>>::const_iterator;
+#pragma link C++ operators vector<pair<vector<TLorentzVector>,vector<int>>>::reverse_iterator;
+#endif
+#endif
